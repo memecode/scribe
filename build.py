425e10c8b82bb96ef5f3eb75582efbe4a608df89
Matthew Allen <fret@memecode.com>
2022-05-21 15:09:27 +1000

    Scribe build fixes.

---
+++ build.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import subprocess
+import urllib.request
+import shutil
+
+print("Build paths:")
+trunk = os.path.abspath(os.path.join(__file__, ".."))
+print("    trunk:     ", trunk)
+scribe = os.path.abspath(os.path.join(trunk, ".."))
+print("    scribe:    ", scribe)
+scribeLibs = os.path.join(scribe, "libs")
+print("    scribeLibs:", scribeLibs)
+code = os.path.abspath(os.path.join(scribe, ".."))
+print("    code:      ", code)
+codeLib = os.path.abspath(os.path.join(code, "../codelib"))
+libpng = os.path.join(codeLib, "libpng")
+zlib = os.path.join(codeLib, "zlib")
+libjpeg = os.path.join(codeLib, "libjpeg-9a")
+print("    codeLib:   ", codeLib)
+lgi = os.path.abspath(os.path.join(code, "lgi/trunk"))
+print("    lgi:       ", lgi)
+
+def Clone(repo, folder):
+	if not os.path.exists(folder):
+		args = ["hg", "clone", repo, folder]
+		print("    Cloning:", repo)
+		p = subprocess.run(args,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.STDOUT)
+		if p.returncode != 0:
+			print("Cmd:", " ".join(args), "failed with:")
+			print(p.stdout.decode())
+			sys.exit(1)
+		print("    ...cloned ok")
+	else:
+		print("   ", folder, "alread exists.")
+
+def Download(url, folder):
+	p = url.split("/")
+	leaf = p[-1]
+	out = os.path.join(folder, leaf)
+	if not os.path.exists(out):
+		print("    Downloading:", out)
+		urllib.request.urlretrieve(url, out)
+		print("    ...downloaded")
+	else:
+		print("    Downloaded:", out)
+	return out
+
+def Extract(file, path):
+	shutil.unpack_archive(file, path)
+
+def MakeIconv(url):
+	archive = Download(url, codeLib)
+	leaf = url.split("/")[-1].replace(".tar.gz", "")
+	oldPath = os.path.join(codeLib, leaf)
+	newPath = os.path.join(codeLib, "libiconv")
+	if not os.path.exists(newPath):
+		Extract(archive, codeLib)
+		os.rename(oldPath, newPath)
+	iconv_h = os.path.join(newPath, "include", "iconv.h")
+	if not os.path.exists(iconv_h):
+		print("Build iconv somehow")
+		sys.exit(1)
+
+print("\nChecking repos:")
+Clone("https://phab.mallen.id.au/diffusion/15/scribelibs/", scribeLibs)
+Clone("https://phab.mallen.id.au/source/lgi/", lgi)
+Clone("https://phab.mallen.id.au/source/libpng/", libpng)
+Clone("https://phab.mallen.id.au/diffusion/10/zlib/", zlib)
+Clone("https://phab.mallen.id.au/diffusion/11/libjpeg/", libjpeg)
+MakeIconv("https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz")
+
+print("Building dependencies:")
+if os.path.exists(os.path.join(scribeLibs, "build-x64")):
+	print("    Seems to be already built.")
+else:
+	args = ["python", "build.py"]
+	p = subprocess.run(args,
+		cwd=scribeLibs)
+
+print("Building scribe:")
+args = ["devenv.com", os.path.join(trunk, "Windows\Scribe_vs2019.sln"), "/Build", "Debug"]
+p = subprocess.run(args, cwd=trunk)
